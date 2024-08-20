e513ce23ea27dd57cb224858a257bbdf54e31f3d
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2024-08-20 15:39:18 -0400

    DAY4

--- prac.py
+++ prac.py
@@ -1,13 +1,4 @@
-def subsets(nums):
-        res = []
-        def dfs(pointer, curr):
-            if pointer>=len(nums):
-                res.append(curr[:])
-                return
-            curr.append(nums[pointer])
-            dfs(pointer+1, curr)
-            curr.pop()
-            dfs(pointer+1, curr)
-        dfs( 0, [])
-        return res
-a = subsets([1,2,3])
+a = 12^9
+print(a)
+b = a^12
+print(b)
