b8703e9cd192adb4c36653b3cff1fbe54d507f97
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2024-08-23 21:52:31 -0400

    DAY6

--- DAY6/InsertIntervals.py
+++ DAY6/InsertIntervals.py
@@ -10,4 +10,22 @@ class Solution:
             else:
                 res.append(newInterval)
         return res
+# =================================================================================
+# 2nd method
+# class Solution:
+#     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+#         l = 0 
+#         res = []
+#         while l<len(intervals):
+#             if newInterval[1]<intervals[l][0]:
+#                 res.append(newInterval)
+#                 return res+intervals[l:]
+#             elif intervals[l][1]<newInterval[0]:
+#                 res.append(intervals[l])
+#             else:
+#                 newInterval = [min(newInterval[0], intervals[l][0]), max(newInterval[1], intervals[l][1])]
+#             l+=1
+        
+#         res.append(newInterval)
+#         return res
         
